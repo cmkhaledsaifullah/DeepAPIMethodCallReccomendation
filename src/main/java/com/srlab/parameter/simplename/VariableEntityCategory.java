4a3de2a29a7bf61bb6ef8247ef6ebd7c5d321771
C M Khaled Saifullah <khaledkucse@gmail.com>
2019-01-02 18:13:41 -0600

    Delete VariableEntityCategory.java

--- src/main/java/com/srlab/parameter/simplename/VariableEntityCategory.java
+++
@@ -1,5 +0,0 @@
-package com.srlab.parameter.simplename;
-
-public enum VariableEntityCategory { //a variable can be one of the following types 
-	LOCAL, FIELD, INHERITED_FIELD, PARAMETER, UNKNOWN;
-}
